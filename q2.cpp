159af7d674e3d4cee858b978ad8998f372ce7d8f
dhananjay-25 <115335510+dhananjay-25@users.noreply.github.com>
2023-02-14 21:56:00 +0530

    Add files via upload

---
+++ q2.cpp
@@ -0,0 +1,9 @@
+//Q2 - Print the ASCII value of character ‘U’.
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c='U';
+    cout<<"ASCII value of U is = "<<int(c);
+    return 0;
+}
